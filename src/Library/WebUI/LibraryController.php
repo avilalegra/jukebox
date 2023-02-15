8059b1d26da25a66415bb9b489b8349ae8def83d
Ricardo Avila <avilalegra@gmail.com>
2023-02-15 17:34:16 +0100

    temporary import button

--- src/Library/WebUI/LibraryController.php
+++ src/Library/WebUI/LibraryController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Library\WebUI;
 
 use App\Library\Application\AudioLibraryInterface;
+use App\Library\Application\AudiosFolderImporter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class LibraryController extends AbstractController
 {
     public function __construct(
-        private AudioLibraryInterface $audioBrowser
-    ) {
+        private AudioLibraryInterface $audioBrowser,
+        private string                $importSourceFolder,
+        private AudiosFolderImporter  $audiosImporter
+    )
+    {
     }
 
     #[Route('/albums', name: 'albums')]
@@ -23,5 +27,19 @@ class LibraryController extends AbstractController
         $albumNames = $this->audioBrowser->albumNamesIndex();
 
         return $this->render('library/albums.html.twig', compact('albumNames'));
+    }
+
+
+    #[Route('/audios/source', name: 'source', methods: ['POST'])]
+    public function importAudios(): Response
+    {
+        try {
+            $this->audiosImporter->importAudios($this->importSourceFolder);
+
+        } catch (\Throwable $t) {
+            dd($t);
+        }
+
+        return $this->redirectToRoute('library.albums');
     }
 }
