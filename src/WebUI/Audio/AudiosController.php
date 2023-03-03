3deffe5a382b81e168920f5f4f07da778db36390
Ricardo Avila <avilalegra@gmail.com>
2023-03-03 10:56:50 +0100

    ImportController

--- src/WebUI/Audio/AudiosController.php
+++ src/WebUI/Audio/AudiosController.php
@@ -1,15 +1,13 @@
 <?php
 
-namespace App\WebUI;
+namespace App\WebUI\Audio;
 
 use App\Audio\Application\Interactor\AudioImporterInterface;
 use App\Audio\Application\Interactor\AudioInfoProviderInterface;
-use App\Form\AudioImportSourceType;
 use App\Player\Application\Interactor\PlayerQueueInterface;
 use App\Player\Application\Interactor\PlayerStatusInfoProviderInterface;
 use App\Shared\WebUI\PaginationParamsTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -81,36 +79,5 @@ class AudiosController extends AbstractController
         }
 
         return $this->redirectToRoute('audios.index');
-    }
-
-
-    #[Route('/library', name: 'import', methods: ['get', 'post'])]
-    public function uploadAudios(Request $request): Response
-    {
-        $form = ($this->createFormBuilder())
-            ->add('file', AudioImportSourceType::class)
-            ->getForm();
-
-        $form->handleRequest($request);
-        $errors = [];
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var UploadedFile $file */
-            $file = $form->get('file')->getData();
-            $file = $file->move('/tmp', $file->getClientOriginalName());
-
-            $importResult = $this->audioImporter->import($file->getRealPath());
-
-            if ($importResult->ok()) {
-                return $this->redirectToRoute('audios.index');
-            }
-
-            $errors = $importResult->errors;
-        }
-
-        return $this->render('audio/import_audios.html.twig', [
-            'form' => $form->createView(),
-            'errors' => $errors
-        ]);
     }
 }
---
+++ src/WebUI/Audio/ImportController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\WebUI\Audio;
+
+use App\Audio\Application\Interactor\AudioImporterInterface;
+use App\Audio\Application\Interactor\AudioInfoProviderInterface;
+use App\Form\AudioImportSourceType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/audios', name: 'audios.')]
+class ImportController extends AbstractController
+{
+    public function __construct(
+        private AudioImporterInterface $audioImporter
+    )
+    {
+    }
+
+    #[Route('/library', name: 'import', methods: ['get', 'post'])]
+    public function uploadAudios(Request $request): Response
+    {
+        $form = ($this->createFormBuilder())
+            ->add('file', AudioImportSourceType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+        $errors = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $file */
+            $file = $form->get('file')->getData();
+            $file = $file->move('/tmp', $file->getClientOriginalName());
+
+            $importResult = $this->audioImporter->import($file->getRealPath());
+
+            if ($importResult->ok()) {
+                return $this->redirectToRoute('audios.index');
+            }
+
+            $errors = $importResult->errors;
+        }
+
+        return $this->render('audio/import_audios.html.twig', [
+            'form' => $form->createView(),
+            'errors' => $errors
+        ]);
+    }
+}
